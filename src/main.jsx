da2c5a63df5f5ed40362e95278d9131621aa1687
afkar-007 <afkar3547@gmail.com>
2026-09-23 18:37:15 +0530

    Update frontend changes

--- src/main.jsx
+++ src/main.jsx
@@ -2,13 +2,16 @@
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import ScrollToTop from './Pages/ScrollToTop'
 import {BrowserRouter} from "react-router-dom"
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap-icons/font/bootstrap-icons.css";
 
+
 createRoot(document.getElementById('root')).render(
 
  <BrowserRouter>
+ <ScrollToTop/>
     <App />
  </BrowserRouter>
   
